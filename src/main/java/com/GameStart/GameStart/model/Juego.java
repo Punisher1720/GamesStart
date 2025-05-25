951a0b184264459217e4106b0f772a00920f19da
Cristobal <oyarzuncristobal353@gmail.com>
2025-05-24 23:19:35 -0400

    a
     a

---
+++ src/main/java/com/GameStart/GameStart/model/Juego.java
@@ -0,0 +1,12 @@
+package com.GameStart.GameStart.model;
+
+public class Juego {
+
+    private int cod_juego;
+    private String nombre_juego;
+    private String descripcion_juego;
+    private int stock_juego;
+    private int precio_juego;
+    private int descuento_id_juego;
+    private int venta_id_venta;
+}
