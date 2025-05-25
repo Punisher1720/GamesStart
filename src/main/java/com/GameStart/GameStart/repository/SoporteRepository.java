e03fe3edaa38d8c38fa4e27a6e01e9c58ffd8fbe
Fernan <fer.diazo@duocuc.cl>
2025-05-25 03:21:23 -0400

    agregada
    lol

--- src/main/java/com/GameStart/GameStart/repository/SoporteRepository.java
+++ src/main/java/com/GameStart/GameStart/repository/SoporteRepository.java
@@ -3,8 +3,10 @@ package com.GameStart.GameStart.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.GameStart.GameStart.model.Soporte;
 
+@Repository
 public interface SoporteRepository extends JpaRepository<Soporte, Long> {
 }
