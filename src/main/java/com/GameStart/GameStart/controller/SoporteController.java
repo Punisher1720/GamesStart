3005a3c905fb676ce8946e3fc0937f3443819d15
Fernan <fer.diazo@duocuc.cl>
2025-05-26 20:51:20 -0400

    delete y modificar

--- src/main/java/com/GameStart/GameStart/controller/SoporteController.java
+++ src/main/java/com/GameStart/GameStart/controller/SoporteController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +42,32 @@ public class SoporteController {
         try {
             Soporte soporte = soporteService.findById(id);
             return ResponseEntity.ok(soporte);
+        } catch ( Exception e ) {
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Soporte> actualizar(@PathVariable Integer id, @RequestBody Soporte soporte) {
+        try {
+            Soporte sop = soporteService.findById(id);
+            sop.setId_ticket(id);
+            sop.setRazon_ticket(soporte.getRazon_ticket());
+            sop.setDesc_msg_ticket(soporte.getDesc_msg_ticket());
+            sop.setRespuesta_msg(soporte.getRespuesta_msg());
+
+            soporteService.save(sop);
+            return ResponseEntity.ok(soporte);
+        } catch ( Exception e ) {
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        try {
+            soporteService.delete(id);
+            return ResponseEntity.noContent().build();
         } catch ( Exception e ) {
             return  ResponseEntity.notFound().build();
         }
