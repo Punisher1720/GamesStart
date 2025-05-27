517c6a794c03da2328487e4f80888e2c4bd6cf48
Cristobal <oyarzuncristobal353@gmail.com>
2025-05-26 20:13:39 -0400

    Update JuegoRepository.java

--- src/main/java/com/GameStart/GameStart/repository/JuegoRepository.java
+++ src/main/java/com/GameStart/GameStart/repository/JuegoRepository.java
@@ -1,11 +1,12 @@
 package com.GameStart.GameStart.repository;
 
-import com.GameStart.GameStart.model.Descuento;
+import com.GameStart.GameStart.model.Juego;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface JuegoRepository extends JpaRepository<Descuento, Long> {
+public interface JuegoRepository extends JpaRepository<Juego, Long> {
 
 }
