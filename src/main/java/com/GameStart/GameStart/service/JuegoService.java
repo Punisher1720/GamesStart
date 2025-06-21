2d985a79c7de2a2801811342c65b883cdafb0470
Cristobal <oyarzuncristobal353@gmail.com>
2025-06-21 16:58:34 -0400

    Update JuegoService.java

--- src/main/java/com/GameStart/GameStart/service/JuegoService.java
+++ src/main/java/com/GameStart/GameStart/service/JuegoService.java
@@ -5,12 +5,18 @@ import com.GameStart.GameStart.Repository.JuegoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
+import java.util.List;
 
 @Service
 public class JuegoService {
 
     @Autowired
     private JuegoRepository juegoRepository;
+
+
+    public List<Juego> findAll() {
+        return juegoRepository.findAll();
+    }
 
     public Optional<Juego> buscarPorId(Long id) {
         return juegoRepository.findById(id);
